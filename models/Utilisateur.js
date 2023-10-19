7bc5f076367db8e7c7852ca8993d25a6c09cc3ff
Hamadi <hamaditimboliba54@gmail.com>
2023-10-19 22:44:12 +0100

    Commit002

--- models/Utilisateur.js
+++ models/Utilisateur.js
@@ -4,7 +4,10 @@ const utlisateurSchema = mongoose.Schema({
     password: { type: String, required: true },
     email: { type: String, required: true },
     nom: { type: String, required: true },
-    username: { type: String, required: true }
+    username: { type: String, required: true },
+    prenom: { type: String, required: true },
+    description: { type: String, required: true },
+    telephone: { type: String, required: true },
     
 })
 module.exports = mongoose.model('Utlisateur',utlisateurSchema)
